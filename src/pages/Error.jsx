b72e982d677abc6e75bc74dbe169e6f0392ee3dc
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-27 21:35:21 +0530

    impvs

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -8,12 +8,12 @@ function Error() {
 
   return (
     <div className="py-32 flex flex-col justify-center items-center">
-      <div className="poppins text-[2.25rem] font-bold">404</div>
-      <div className="mt-4 inter text-[1rem] font-normal">
+      <div className="poppins text-[1.75rem] font-bold">404</div>
+      <div className="mt-4 inter text-[0.9rem] font-normal">
         The page you’re looking for doesn’t exist.
       </div>
       <IconButton
-        className="mt-16"
+        className="mt-8"
         iconSrc={leftArrow}
         text="Go Home"
         onClick={() => navigate("/")}
