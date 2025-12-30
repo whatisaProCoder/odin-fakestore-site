c7bc854af6b1ccfe2ca7554700e5d9f1bcbc43c7
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-31 01:05:03 +0530

    hero section finally done

--- src/components/common/Carousel.jsx
+++ src/components/common/Carousel.jsx
@@ -10,6 +10,19 @@ function Carousel({
   height,
   width,
 }) {
+  const nextItem = () => {
+    setItemIndex((index) => (index + 1) % itemArray.length);
+  };
+  const prevItem = () => {
+    setItemIndex((index) => {
+      if (index == 0) {
+        return itemArray.length - 1;
+      } else {
+        return index - 1;
+      }
+    });
+  };
+
   return (
     <div
       className={
@@ -20,10 +33,16 @@ function Carousel({
     >
       <div className="w-full flex-1 flex flex-col rounded-sm rounded-b-none overflow-hidden relative">
         <div className="absolute top-0 bottom-0 left-0 right-0 flex flex-row justify-between items-center px-2 z-1">
-          <button className="bg-[#1b1d2098] backdrop-blur-md pr-0.5 rounded-full aspect-square h-8 flex flex-row justify-center items-center transition-colors hover:bg-[#2573e9d2]">
+          <button
+            onClick={prevItem}
+            className="bg-[#1b1d2098] backdrop-blur-md pr-0.5 rounded-full aspect-square h-8 flex flex-row justify-center items-center transition-colors hover:bg-[#2573e9d2]"
+          >
             <img src={leftArrowWhite} />
           </button>
-          <button className="bg-[#1b1d2098] backdrop-blur-md pl-0.5 rounded-full aspect-square h-8 flex flex-row justify-center items-center transition-colors hover:bg-[#2573e9d2]">
+          <button
+            onClick={nextItem}
+            className="bg-[#1b1d2098] backdrop-blur-md pl-0.5 rounded-full aspect-square h-8 flex flex-row justify-center items-center transition-colors hover:bg-[#2573e9d2]"
+          >
             <img src={rightArrowWhite} />
           </button>
         </div>
@@ -40,10 +59,7 @@ function Carousel({
           percentage={`${(itemIndex / (itemArray.length - 1)) * 100}%`}
         />
       </div>
-      <div
-        key={itemIndex}
-        className="mt-2 text-center inter font-medium fade-in-fast"
-      >
+      <div key={itemIndex} className="mt-2 text-center inter fade-in-fast">
         {itemArray[itemIndex].name}
       </div>
     </div>
