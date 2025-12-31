83ae98c0345834fd1b9fcfc9d8f2e6f492e7a2e3
whatisaProCoder <debnathpritam2005@gmail.com>
2025-12-31 21:47:12 +0530

    imvs

--- src/components/home/ExclusiveDealsCard.jsx
+++ src/components/home/ExclusiveDealsCard.jsx
@@ -5,11 +5,11 @@ function ExclusiveDealsCard() {
     <div className="bg-[#1b1d20] flex flex-col p-2 border border-[#32333fb5] rounded-md max-sm:rounded-xl transition-all hover:drop-shadow-xl hover:-translate-y-1">
       <img src={shirtExample} className="rounded-xs max-sm:rounded-md" />
       <div className="flex flex-col gap-4 p-3">
-        <div className="poppins text-[1rem] max-sm:text-sm font-normal">
+        <div className="inter text-[1rem] max-sm:text-sm font-semibold">
           Sleek Olive Green Hardshell Carry-On Luggage
         </div>
         <div className="flex flex-row justify-between items-center">
-          <div className="poppins text-md max-sm:text-[0.9rem] font-bold">
+          <div className="inter text-md max-sm:text-[0.9rem] font-bold">
             $ 48
           </div>
           <div className="select-none px-3 py-1 bg-[#182436] border border-[#2573E9] text-[#2573E9] rounded-2xl poppins text-[0.9rem] max-sm:text-[0.8rem] font-medium transition-colors hover:text-[#5397fe] hover:border-[#5397fe]">
