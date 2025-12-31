29f85fba4950a3d9511f074b0848e80471ad8048
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-01 02:14:29 +0530

    fix

--- src/components/home/CategoryCard.jsx
+++ src/components/home/CategoryCard.jsx
@@ -1,6 +1,6 @@
 function CategoryCard({ categoryImage, name }) {
   return (
-    <div className="flex flex-col justify-center px-4 py-6 items-center border border-[#202227] rounded-2xl transition-all duration-300 bg-[#14161a] hover:bg-[#1b1d20]">
+    <div className="select-none flex flex-col justify-center px-4 py-6 items-center border border-[#202227] rounded-2xl transition-all duration-300 bg-[#14161a] hover:bg-[#1b1d20]">
       <img src={categoryImage} alt={name} className="w-20 max-sm:w-18" />
       <div className="mt-6 inter font-medium text-center max-sm:text-sm">
         {name}
