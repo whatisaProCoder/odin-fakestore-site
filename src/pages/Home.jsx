27034e2814719a97ac395db150ee7ddc075b5caa
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-02 14:11:06 +0530

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import CallToAction from "../components/home/CallToAction";
 import Categories from "../components/home/Categories";
 import ExclusiveDeals from "../components/home/ExclusiveDeals";
@@ -6,19 +5,6 @@ import Features from "../components/home/Features";
 import Hero from "../components/home/Hero";
 
 function Home() {
-  const scrollToTop = () => {
-    setTimeout(() => {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }, 300);
-  };
-
-  useEffect(() => {
-    scrollToTop();
-  }, []);
-
   return (
     <>
       <Hero />
