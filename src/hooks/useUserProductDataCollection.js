b33ff260791f18d96882151479a67d0b40198708
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-02 17:14:21 +0530

    useUserProductDataCollection custom hook first iteration done

--- src/hooks/useUserProductDataCollection.js
+++ src/hooks/useUserProductDataCollection.js
@@ -1,5 +1,99 @@
-function useUserProductDataCollection() {
+import { useState } from "react";
+import UserProductData from "../models/userProductData";
 
+function useUserProductDataCollection() {
+  const [userProductDataCollection, setUserProductDataCollection] = useState(
+    []
+  );
+
+  const getProductData = ({ productID }) => {
+    const productData = userProductDataCollection.find(
+      (product) => product.productID === productID
+    );
+    return productData;
+  }
+
+  const toggleAddToCartState = (productID) => {
+    const productData = getProductData(productID);
+    if (productData) {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      newUserProductDataCollection.find(
+        (product) => product.productID === productID
+      ).addedToCart = !productData.addedToCart;
+      setUserProductDataCollection(newUserProductDataCollection);
+    } else {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      newUserProductDataCollection.push(
+        new UserProductData({
+          productID: productID,
+          addedToCart: true,
+          addedToWishlist: false,
+          count: 1,
+        })
+      );
+      setUserProductDataCollection(newUserProductDataCollection);
+    }
+  };
+
+  const toggleWishlistState = (productID) => {
+    const productData = getProductData(productID);
+    if (productData) {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      newUserProductDataCollection.find(
+        (product) => product.productID === productID
+      ).addedToWishlist = !productData.addedToWishlist;
+      setUserProductDataCollection(newUserProductDataCollection);
+    } else {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      newUserProductDataCollection.push(
+        new UserProductData({
+          productID: productID,
+          addedToCart: false,
+          addedToWishlist: true,
+          count: 1,
+        })
+      );
+      setUserProductDataCollection(newUserProductDataCollection);
+    }
+  }
+
+  const handleCount = ({ productID, minus = false } = {}) => {
+    const productData = getProductData(productID);
+    if (productData) {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      let newCount = productData.count;
+      if (minus && productData.count > 1) {
+        newCount--;
+      } else if (!minus && productData.count <= 7) {
+        newCount++;
+      }
+      newUserProductDataCollection.find(
+        (product) => product.productID === productID
+      ).count = newCount;
+      setUserProductDataCollection(newUserProductDataCollection);
+    } else {
+      let newUserProductDataCollection = [...userProductDataCollection];
+      newUserProductDataCollection.push(
+        new UserProductData({
+          productID: productID,
+          addedToCart: false,
+          addedToWishlist: false,
+          count: minus ? 1 : 2,
+        })
+      );
+      setUserProductDataCollection(newUserProductDataCollection);
+    }
+  };
+
+  const getProductIDsInCart = () => {
+    return userProductDataCollection.filter(product => product.addedToCart === true)
+  }
+
+  const getProductIDsInWishlist = () => {
+    return userProductDataCollection.filter(product => product.addedToWishlist === true)
+  }
+
+  return { getProductData, toggleAddToCartState, toggleWishlistState, handleCount, getProductIDsInCart, getProductIDsInWishlist }
 }
 
 export default useUserProductDataCollection;
