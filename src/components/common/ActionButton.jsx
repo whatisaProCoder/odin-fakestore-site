abda5c03e0a5b930f4e234e52577b84d7dc9c6c2
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-03 16:50:14 +0530

    fix

--- src/components/common/ActionButton.jsx
+++ src/components/common/ActionButton.jsx
@@ -4,7 +4,7 @@ function ActionButton({ text, onClick, className, disabled }) {
       onClick={onClick}
       disabled={disabled}
       className={
-        "flex flex-row white-bg px-9 py-3 gap-3 rounded-4xl transition-all hover:gap-5 " +
+        "flex flex-row bg-[rgba(255,255,255,0.87)] hover:bg-[rgba(255,255,255,0.77)] active:bg-[rgba(255,255,255,0.5)] px-9 py-3 gap-3 rounded-4xl transition-all hover:gap-5 " +
         className
       }
     >
