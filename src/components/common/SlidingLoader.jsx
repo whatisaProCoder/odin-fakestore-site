c1dda6f06963deb0510ea9bf67b1fdc1558a6aa3
whatisaProCoder <debnathpritam2005@gmail.com>
2026-01-03 23:37:10 +0530

    make sliding loader border curved

--- src/components/common/SlidingLoader.jsx
+++ src/components/common/SlidingLoader.jsx
@@ -1,6 +1,6 @@
 function SlidingLoader({ percentage }) {
   return (
-    <div className="bg-[#242C37] w-full h-1">
+    <div className="bg-[#242C37] w-full h-1 rounded-b-xs overflow-clip">
       <div
         className="h-full bg-[#2573E9] transition-all"
         style={{ width: percentage }}
